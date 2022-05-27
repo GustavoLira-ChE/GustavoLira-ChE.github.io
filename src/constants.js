1801e7bf5f113c01a4f0cd481e40f29a71718643
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-27 14:40:06 -0500

    fix: some warning about syntax

--- src/constants.js
+++ src/constants.js
@@ -1,26 +1,26 @@
 export const COLORS = {
-    white: '0, 0%, 100%', //Background
-    gray: '0deg 0% 62%', // text
-    primary: '240deg 60% 63%', //blue
-    secondary: '169deg 80% 40%', //green
-    black: '0deg 0% 11%' // footer
-  };
+    white: "0, 0%, 100%", //Background
+    gray: "0deg 0% 62%", // text
+    primary: "240deg 60% 63%", //blue
+    secondary: "169deg 80% 40%", //green
+    black: "0deg 0% 11%" // footer
+};
   
-  export const WEIGHTS = {
+export const WEIGHTS = {
     normal: 500,
     medium: 600,
     bold: 800,
-  };
+};
   
-  export const BREAKPOINTS = {
+export const BREAKPOINTS = {
     phone: 600,
     tablet: 950,
     laptop: 1300,
-  };
+};
   
-  export const QUERIES = {
+export const QUERIES = {
     phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
     tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
     laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
-  };
+};
   
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./routes/App";
 
-const root = createRoot(document.getElementById("app"));
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
 root.render(<App />);
