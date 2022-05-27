0099492a65504ba38c04c557c4ef70de4dde2b56
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-27 13:46:53 -0500

    chore: added configurations to webpack config file as production

---
+++ webpack.prod.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPluginReact = require('eslint-plugin-react');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },{
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new EslintPluginReact(),
+    ]
+}
