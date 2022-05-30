aea2fec8b259125d20ecdbfe5f52ac4c7116cb1c
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-30 16:16:19 -0500

    fix: global styles custom colors

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const COLORS = {
     white: "0, 0%, 100%", //Background
     gray: "0deg 0% 62%", // text
-    primary: "240deg 60% 63%", //blue
+    primary: "228deg 66% 53%", //blue
     secondary: "169deg 80% 40%", //green
     black: "0deg 0% 11%" // footer
 };
