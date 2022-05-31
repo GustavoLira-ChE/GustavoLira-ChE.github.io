8fb93239347cb6e12435d12368ed090b3041647a
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-31 16:44:43 -0500

    feat: add main styles and content to welcome page

--- src/containers/MainContent.jsx
+++ src/containers/MainContent.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from "styled-components/macro"
 
 const MainContent = () => {
     return (
-        <div>
-            <h2>Main Content</h2>
-        </div>
+        <MainContentWrapper>
+            <WelcomeWrapper>
+                <p>Hello I'm Gustavo </p>
+                <p>a <strong>software developer</strong> and chemical engineer.</p>
+            </WelcomeWrapper>
+        </MainContentWrapper>
     );
 };
+
+const MainContentWrapper = styled.div`
+  height: 90vh;
+  background-color: var(--color-primary);
+  display: grid;
+  place-content: center;
+`
+const WelcomeWrapper = styled.div`
+  color: var(--color-white);
+  text-align: center;
+  font-size: 2em;
+
+  & strong {
+      color: var(--color-secondary);
+  }
+`
 
 export default MainContent;
