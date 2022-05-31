9171a2a0d1abebf3be7f47a18cead324ac80daef
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-31 16:42:27 -0500

    chore: add more section to Welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import About from '../components/About';
 import Footer from '../containers/Footer';
 import Header from '../containers/Header';
 import MainContent from '../containers/MainContent';
+import ProjectList from '../containers/ProjectList';
 
 const Welcome = () => {
     return (
         <PageWrapper>
             <Header />
             <MainContent />
+            <About />
+            <ProjectList />
             <Footer />
         </PageWrapper>
     );
