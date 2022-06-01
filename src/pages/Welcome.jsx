cc1a8837d8623ca9ea431609bb1a86c669e5aaa7
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-01 14:32:19 -0500

    fix: delete header from welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import About from '../components/About';
 import Footer from '../containers/Footer';
-import Header from '../containers/Header';
 import MainContent from '../containers/MainContent';
 
 const Welcome = () => {
     return (
         <PageWrapper>
-            <Header />
             <MainContent />
             <About />
             <Footer />
