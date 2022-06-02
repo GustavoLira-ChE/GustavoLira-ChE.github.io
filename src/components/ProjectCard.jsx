b9feff46a9401efca9b563add1c873f6c663ff15
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-02 15:44:18 -0500

    style: add style to project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { GoRepo } from "react-icons/go";
 
 const ProjectCard = ( { data } ) => {
     return (
         <CardWrapper>
-            <h3>{data.title}</h3>
+            <TitleCard>{data.title}</TitleCard>
             <p>{data.description}</p>
-            <p>{data.technologies}</p>
+            <p><strong>Build it with:</strong> {data.technologies}</p>
             <LinkRepo>
-                <CardLink href={data.pageLink}>Page Link</CardLink>
-                <CardLink href={data.gitHubRepo}>GitHub Link</CardLink>
+                <CardLink href={data.pageLink} target="_blank">Page Link</CardLink>
+                <CardLink href={data.gitHubRepo} target="_blank"><GoRepoStyled /></CardLink>
             </LinkRepo>
         </CardWrapper>
     );
@@ -17,9 +18,16 @@ const ProjectCard = ( { data } ) => {
 
 const CardWrapper = styled.article`
   padding: 16px;
-  background-color: var(--color-primary);
+  background-color: var(--color-secondary);
   color: var(--color-white);
-  border-radius: 8px;
+  border-radius: 40px 4px 4px 4px;
+  -webkit-box-shadow: 6px 7px 26px -6px rgba(32,64,117,0.98);
+  -moz-box-shadow: 6px 7px 26px -6px rgba(32,64,117,0.98);
+  box-shadow: 6px 7px 26px -6px rgba(32,64,117,0.98);
+`
+const TitleCard = styled.h3`
+  text-align: center;
+  text-decoration: underline;
 `
 const LinkRepo = styled.div`
   display: flex;
@@ -28,6 +36,9 @@ const LinkRepo = styled.div`
 const CardLink = styled.a`
   text-decoration: none;
   color: var(--color-white);
+`
+const GoRepoStyled = styled(GoRepo)`
+  font-size: 1.5rem;
 `
 
 export default ProjectCard;
