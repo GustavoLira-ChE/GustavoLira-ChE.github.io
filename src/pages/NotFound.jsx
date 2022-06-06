afaa83d84b890fe7e74def1610bac4ffec6cbc04
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-06 15:47:59 -0500

    style: center children elements

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { VscTelescope } from "react-icons/vsc";
+
+const Page404 = () => {
+    return (
+        <ErrorPageWrapper>
+            <h2>Looks like this page is missing <strong>:\</strong></h2>
+            <TbError404Styled />
+        </ErrorPageWrapper>
+    );
+};
+
+const ErrorPageWrapper = styled.div`
+  height: 90vh;
+  display: grid;
+  place-content: center;
+  color: var(--color-black);
+  justify-items: center;
+  gap: 16px;
+`
+const TbError404Styled =  styled(VscTelescope)`
+  font-size: 4rem;
+  color: inherit;
+  align-self: center;
+`
+
+export default Page404;
